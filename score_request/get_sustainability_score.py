d0b5a1e63288902938e6e37f662dd451c594e51e
Spig <nicolaischia95@gmail.com>
2023-09-16 15:35:45 +0200

    Imports

--- score_request/get_sustainability_score.py
+++ score_request/get_sustainability_score.py
@@ -1,10 +1,11 @@
 import os
-import numpy as np
-import pandas as pd
 from typing import List
+
+import pandas as pd
 
 ratings = pd.read_csv(os.path.join(os.path.dirname(__file__), "ratings.csv"))
 SUSTAINABILITY_THRESHOLD = 3.5
+
 
 def get_sustainability_score(products_list: List[int]):
     print(f"INFO - get_sustainability_score got products {products_list}")
